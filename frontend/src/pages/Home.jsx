7d1aa962c1506b6efd9a431b40632bb07e30ff2e
Tyler Stevens <tstevens728@yahoo.com>
2025-01-25 18:08:12 -0500

    Completed Notes App

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import api from "../api";
+import Note from "../components/Note"
+import "../styles/Home.css"
 
 function Home() {
   const [notes, setNotes] = useState([]);
@@ -49,6 +51,9 @@ function Home() {
     <div>
       <div>
         <h2>Notes</h2>
+        {notes.map((note) => (
+          <Note note={note} onDelete={deleteNote} key={note.id} />
+        ))}
       </div>
       <h2>Create a Note</h2>
       <form onSubmit={createNote}>
